51e4083867cfacf1497655bd1ceb14139dd98bde
Rajat <prashant.rajat007@gmail.com>
2022-01-14 12:26:25 +0530

    Updated Multiply button

--- calc.js
+++ calc.js
@@ -1,9 +1,19 @@
 let screen = document.getElementById('screen');   //getting the input area
+
 buttons = document.querySelectorAll('button');    // getting the buttons
-let screenValue ='';
+
+let screenValue ='';     // value of input area
+
+
 for(item of buttons){
     item.addEventListener('click' , (f)=>{
         buttonText = f.target.innerText;    // will give you the text  inside of that particulat button
+
+        if(buttonText == 'X'){
+            buttonText = '*';
+            screen.value += buttonText;
+            screen.value = screenValue;
+        }
         
         // For clearing the input area
 
@@ -13,17 +23,17 @@ for(item of buttons){
 
             //for clearing a single input
             
-        }else if (buttonText == 'c') {
+        }else if (buttonText == 'C') {
             screenValue = screen.value.substr(0, screen.value.length-1)     // returns a portion of the string, starting at the specified index and extending for a given number of characters afterwards.
             screen.value = screenValue;
         }
         
         
         else if (buttonText == '=') {
-            screen.value = eval(screenValue)
+            screen.value = eval(screenValue)   //eval() For evaluating The Screen value
             
         } else{
-            screenValue += buttonText;
+            screenValue += buttonText;    //button text will add in screen value
             screen.value = screenValue;
         }
     })
